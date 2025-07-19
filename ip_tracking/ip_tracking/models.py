ec2d1ce48b68945d50a6a3ce70a6ef09f6b67d9a
Nebiyou-x <Nebiyoutad@gmail.com>
2025-07-19 16:40:04 +0300

    block ip done

--- ip_tracking/ip_tracking/models.py
+++ ip_tracking/ip_tracking/models.py
@@ -1,10 +1,7 @@
 # ip_tracking/models.py
-from django.db import models
-
-class RequestLog(models.Model):
-    ip_address = models.CharField(max_length=45)  # Supports both IPv4 and IPv6
-    timestamp = models.DateTimeField(auto_now_add=True)
-    path = models.CharField(max_length=255)
+class BlockedIP(models.Model):
+    ip_address = models.CharField(max_length=45, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.ip_address} - {self.path}"
+        return self.ip_address
